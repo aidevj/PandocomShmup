a61d77de070decf788ccd1882e671bf0c6dbf6af
Isis Joy Melendez <itm5305@rit.edu>
2016-09-13 15:43:51 -0400

    Add files via upload

---
+++ bulletScript.js
@@ -0,0 +1,17 @@
+﻿// public variable, can be directly edited in inspector
+public var speed : int = 8;
+
+// function called once when the bullet is created
+function Start() {
+    // get rigidbody component
+    var r2d = GetComponent("Rigidbody2D");
+
+    // make the bullet move sideways
+    r2d.velocity.x = speed;
+}
+
+// Function called when the object goes out of the screen
+function OnBecameInvisible() { // this is preset name function in unity(?)
+    // destroy the bullet
+    Destroy(gameObject);
+}
---
+++ spaceshipScript.js
@@ -0,0 +1,25 @@
+﻿// A variable taht will contain our bullet prefab
+public var bullet : GameObject;
+
+// Function called about 60 times per second
+function Update() {
+    // Get the rigidbody component
+    var r2d = GetComponent("Rigidbody2D");
+
+    // Move the spaceship when an arrow key is pressed
+    if (Input.GetKey("up"))
+        r2d.velocity.y = 10;
+    else if (Input.GetKey("down"))
+        r2d.velocity.y = -10;
+    else
+        r2d.velocity.y = 0;
+
+    // Bullet firing handling
+    // When space bar is pressed
+    if (Input.GetKeyDown("space")) {
+        // create a new bullet at "transform.position"
+        // which is the current position of the ship
+        // Quaternion.identity = add the bullet with no rotation
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
